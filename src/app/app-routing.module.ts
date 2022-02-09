0c581121819a0a04a758eeee7e7e697fd06ee299
rasadulalam <rashed464@outlook.com>
2022-02-09 15:54:34 +0600

    First release

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:HomeComponent,
+    path:'home'
+  },
+  {
+    component:AboutComponent,
+    path:'about'
+  },
+  {
+    component:ContactComponent,
+    path:'contact'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
