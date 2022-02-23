d161a117c4b74adbbabfe6e72219ababf70c3947
rasadulalam <rashed464@outlook.com>
2022-02-23 08:16:28 +0600

    code change

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
+
 export class FooterComponent implements OnInit {
 
   constructor() { }
